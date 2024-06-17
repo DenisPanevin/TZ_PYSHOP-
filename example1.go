f3b7447d92503d4b0af96d40760d85b247447b4e
DenisPanevin <panevindenisgo@gmail.com>
2024-06-17 11:05:59 +0300

    Create example1.go

---
+++ example1.go
@@ -0,0 +1,48 @@
+func fillScores() *[]ScoreStamp {
+	random := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	scores := make([]ScoreStamp, 0, 10)
+
+	for i := 0; i < STAMPS_NUMBER; i++ {
+		scoreChanged := random.Float32() < PROBABILITY_GOAL
+		home := 0
+		away := 0
+
+		if scoreChanged {
+			if random.Float32() < PROBABILITY_FIRST_TEAM_GOAL {
+				home = 1
+				away = 0
+			} else {
+				home = 0
+				away = 1
+			}
+		}
+
+		var prevScore Score
+		if len(scores) == 0 {
+			prevScore = Score{
+				Home: 0,
+				Away: 0,
+			}
+		} else {
+			prevScore = scores[i-1].Score
+		}
+
+		newScore := Score{
+			Home: prevScore.Home + home,
+			Away: prevScore.Away + away,
+		}
+		scores = append(scores, ScoreStamp{Offset: i, Score: newScore})
+	}
+
+	return &scores
+}
+
+
+func getScore(scores *[]ScoreStamp, offset int) (Score, error) {
+    if offset < 0 || offset >= len(*scores) {
+        return Score{}, fmt.Errorf("offset %d is out of range", offset)
+    }
+
+    return (*scores)[offset].Score, nil
+}
